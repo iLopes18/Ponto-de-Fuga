bc7916ac0c0cea8c720403f0f024de390bc3a781
iLopes18 <18isaaclopes@gmail.com>
2026-06-25 15:53:57 +0100

    feat: update collection II assets
    Update imagery for collection II to refresh the visual content.

--- src/data.ts
+++ src/data.ts
@@ -63,55 +63,55 @@ export const apenasEstarProjects: Project[] = [
   {
     id: 'ae1',
     title: '',
-    image: '/collection_II/1.png',
+    image: '/collection_II/1.webp',
     year: '2026'
   },
   {
     id: 'ae2',
     title: '',
-    image: '/collection_II/2.png',
+    image: '/collection_II/2.webp',
     year: '2026'
   },
   {
     id: 'ae3',
     title: '',
-    image: '/collection_II/3.png',
+    image: '/collection_II/3.webp',
     year: '2026'
   },
   {
     id: 'ae4',
     title: '',
-    image: '/collection_II/4.png',
+    image: '/collection_II/4.webp',
     year: '2026'
   },
   {
     id: 'ae5',
     title: '',
-    image: '/collection_II/5.png',
+    image: '/collection_II/5.webp',
     year: '2026'
   },
   {
     id: 'ae6',
     title: '',
-    image: '/collection_II/6.png',
+    image: '/collection_II/6.webp',
     year: '2026'
   },
   {
     id: 'ae7',
     title: '',
-    image: '/collection_II/7.png',
+    image: '/collection_II/7.webp',
     year: '2026'
   },
   {
     id: 'ae8',
     title: '',
-    image: '/collection_II/8.png',
+    image: '/collection_II/8.webp',
     year: '2026'
   },
   {
     id: 'ae9',
     title: '',
-    image: '/collection_II/9.png',
+    image: '/collection_II/9.webp',
     year: '2026'
   }
 ];
